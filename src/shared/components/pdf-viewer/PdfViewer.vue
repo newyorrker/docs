ea5a8031fa1eaa256fa154a0f5e2ba58978bb5bf
rkarshiev <newyorrker@gmail.com>
2023-01-30 13:17:48 +0300

    update

--- src/shared/components/pdf-viewer/PdfViewer.vue
+++ src/shared/components/pdf-viewer/PdfViewer.vue
@@ -98,46 +98,74 @@ export default class PadViewer extends Vue {
 
     let pinchZoomEnabled = false;
     function enablePinchZoom(pdfViewer: any) {
-        let startX = 0, startY = 0;
+        let startX = 0,
+            startY = 0;
         let initialPinchDistance = 0;
         let pinchScale = 1;
         const viewer = document.getElementById("viewer");
         const container = document.getElementById("viewerContainer");
-        const reset = () => { startX = startY = initialPinchDistance = 0; pinchScale = 1; };
+        const reset = () => {
+            startX = startY = initialPinchDistance = 0;
+            pinchScale = 1;
+        };
         // Prevent native iOS page zoom
         //document.addEventListener("touchmove", (e) => { if (e.scale !== 1) { e.preventDefault(); } }, { passive: false });
         document.addEventListener("touchstart", (e) => {
             if (e.touches.length > 1) {
                 startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
                 startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
-                initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+                initialPinchDistance = Math.hypot(
+                    e.touches[1].pageX - e.touches[0].pageX,
+                    e.touches[1].pageY - e.touches[0].pageY
+                );
             } else {
                 initialPinchDistance = 0;
             }
         });
-        document.addEventListener("touchmove", (e) => {
-            if (initialPinchDistance <= 0 || e.touches.length < 2) { return; }
-            //@ts-ignore
-            if (e.scale !== 1) { e.preventDefault(); }
-            const pinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
-            //@ts-ignore
-            const originX = startX + container.scrollLeft;
-            //@ts-ignore
-            const originY = startY + container.scrollTop;
-            pinchScale = pinchDistance / initialPinchDistance;
-            //@ts-ignore
-            viewer.style.transform = `scale(${pinchScale})`;
-            //@ts-ignore
-            viewer.style.transformOrigin = `${originX}px ${originY}px`;
-        }, { passive: false });
+        document.addEventListener(
+            "touchmove",
+            (e) => {
+                if (initialPinchDistance <= 0 || e.touches.length < 2) {
+                    return;
+                }
+                //@ts-ignore
+                if (e.scale !== 1) {
+                    e.preventDefault();
+                }
+                const pinchDistance = Math.hypot(
+                    e.touches[1].pageX - e.touches[0].pageX,
+                    e.touches[1].pageY - e.touches[0].pageY
+                );
+                //@ts-ignore
+                const originX = startX + container.scrollLeft;
+                //@ts-ignore
+                const originY = startY + container.scrollTop;
+                pinchScale = pinchDistance / initialPinchDistance;
+                //@ts-ignore
+                viewer.style.transform = `scale(${pinchScale})`;
+                //@ts-ignore
+                viewer.style.transformOrigin = `${originX}px ${originY}px`;
+            },
+            { passive: false }
+        );
         document.addEventListener("touchend", (e) => {
-            if (initialPinchDistance <= 0) { return; }
+            if (initialPinchDistance <= 0) {
+                return;
+            }
             //@ts-ignore
             viewer.style.transform = `none`;
             //@ts-ignore
             viewer.style.transformOrigin = `unset`;
-            //@ts-ignore
             pdfViewer.currentScale *= pinchScale;
+
+            const scaleFactor = pdfViewer.currentScale;
+
+            if (scaleFactor < 1) {
+              pdfViewer.zoomOut(null, scaleFactor);
+            } else if (scaleFactor > 1) {
+              pdfViewer.zoomIn(null, scaleFactor);
+            }
+
             //@ts-ignore
             const rect = container.getBoundingClientRect();
             const dx = startX - rect.left;
@@ -146,15 +174,13 @@ export default class PadViewer extends Vue {
             container.scrollLeft += dx * (pinchScale - 1);
             //@ts-ignore
             container.scrollTop += dy * (pinchScale - 1);
-
             reset();
         });
     }
-    document.addEventListener('webviewerloaded', () => {
+    document.addEventListener("DOMContentLoaded", () => {
         if (!pinchZoomEnabled) {
             pinchZoomEnabled = true;
-            //@ts-ignore
-            enablePinchZoom();
+            enablePinchZoom(pdfViewer);
         }
     });
 
