69af5c9543ee6fb63780844c83c29e4dec6c36d4
rkarshiev <newyorrker@gmail.com>
2023-02-03 13:11:19 +0300

    update

--- src/entries/applications.ts
+++ src/entries/applications.ts
@@ -16,6 +16,6 @@ app.$mount('#app');
 
 if (process.env.NODE_ENV !== 'production') {
   window.sessionFromNative(
-    '{"backendUrl":"https://api.test.appercode.com/hrlink", "sessionId":"4003303e-9d4e-487b-af68-8c07d839d5b4", "userId": 41323, "baseUrl":"https://api.test.appercode.com/","projectName":"hrlink","appPlatform":"Web","appVersion": "3.3.0","language": "ru", "userProfile": {"groupIds":["eadb87e0-538e-49d6-b985-40986f8e3e3b"]}}',
+    '{"backendUrl":"https://api.test.appercode.com/hrlink", "sessionId":"0ad2c75a-b9dd-46a2-a8b9-9a91bae83fdc", "userId": 41323, "baseUrl":"https://api.test.appercode.com/","projectName":"hrlink","appPlatform":"Web","appVersion": "3.3.0","language": "ru", "userProfile": {"groupIds":["eadb87e0-538e-49d6-b985-40986f8e3e3b"]}}',
   );
 }
